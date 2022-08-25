860f8aceb71d5b477ac79f2ccaf418d4971ca52b
theofremiot <theofremiot@gmail.com>
2022-08-26 00:32:37 +0200

    ajout de la possibilité d'exporter une photo

--- src/image_processing.py
+++ src/image_processing.py
@@ -25,17 +25,24 @@ def add_noise_on_image(img, noise, k):
     return img
 
 
+def export_image(img, filename):
+    final = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_BGR2RGB)
+    cv2.imwrite(filename, final)
+
+
 if __name__ == '__main__':
     print("image processing...")
-    photo_or = get_image("data/photo_identitee.jpg")
+    photo_or = get_image("data/photo.jpg")
     photo = photo_or.copy()
     h, l, n = photo.shape
     noise = create_gaussian_noise_mat(h, l)
     photo_w_noise = add_noise_on_image(photo, noise, 0.01)
     plt.figure()
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.imshow(photo_or)
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.imshow(photo_w_noise)
-    plt.show()
+    plt.subplot(1, 3, 3)
+    plt.imshow(abs(photo_w_noise-photo_or) * int(255/np.max(abs(photo_w_noise-photo_or))))
+    # plt.show()
 
